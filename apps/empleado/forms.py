57bd3661fc116ef5b5f2cb3f782519a79f8231f7
sebascastano <castano.sebastian@correounivalle.edu.co>
2018-09-21 11:44:25 -0500

    Funcionalidad agregar area, lista

---
+++ apps/empleado/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Area
+
+
+
+# ---------------------AREA----------------------------------------------------------
+
+class AgregarAreaForm(forms.ModelForm):
+    descripcion = forms.CharField(widget=forms.Textarea, label="Descripción")
+
+    def __init__(self, *args, **kwargs):
+        super(AgregarAreaForm, self).__init__(*args, **kwargs)
+        # PlaceHolders
+        self.fields['nombre'].widget.attrs['placeholder'] = "Ej. Fisioterapia"
+        self.fields['descripcion'].widget.attrs[
+            'placeholder'] = "Ej. La fisioterapia, también conocida como terapia física,​ es una disciplina de la salud que ofrece una " \
+                             "alternativa terapéutica no farmacológica para diagnosticar, prevenir y tratar síntomas de múltiples dolencias, " \
+                             "tanto agudas como crónicas, por medio del ejercicio terapéutico, calor, frío, luz, agua, técnicas manuales " \
+                             "entre ellas la electricidad."
+
+    class Meta:
+        model = Area
+        fields = ('nombre', 'descripcion')
+
