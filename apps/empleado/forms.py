3381a5db47370ff53e951a1e527725c46629b5b1
sebascastano <castano.sebastian@correounivalle.edu.co>
2018-09-21 16:52:47 -0500

    Se agrega la funcionalidad de modificar area

--- apps/empleado/forms.py
+++ apps/empleado/forms.py
@@ -22,3 +22,17 @@ class AgregarAreaForm(forms.ModelForm):
         model = Area
         fields = ('nombre', 'descripcion')
 
+class ModificarAreaForm(forms.ModelForm):
+    descripcion = forms.CharField(widget=forms.Textarea, label="Descripción")
+
+    def __init__(self, *args, **kwargs):
+        super(ModificarAreaForm, self).__init__(*args, **kwargs)
+        # PlaceHolders
+        self.fields['nombre'].widget.attrs['placeholder'] = "Ej. Fisioterapia"
+        self.fields['descripcion'].widget.attrs[
+            'placeholder'] = "Ej. La fisioterapia, también conocida como terapia física,​ es una disciplina de la salud que ofrece una " \
+                             "alternativa terapéutica no farmacológica para diagnosticar, prevenir y tratar síntomas de múltiples dolencias, " \
+                             "tanto agudas como crónicas, por medio del ejercicio terapéutico, calor, frío, luz, agua, técnicas manuales "
+    class Meta:
+        model = Area
+        fields = ('nombre', 'descripcion')
--- apps/empleado/urls.py
+++ apps/empleado/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.empleado.views import agregar_area, consultar_area
+from apps.empleado.views import agregar_area, consultar_area, modificar_area
 
 
 urlpatterns = [
     path('agregar_area/', agregar_area, name='agregar_area'),
-    path('consultar_area/', consultar_area, name='consultar_area')
+    path('consultar_area/', consultar_area, name='consultar_area'),
+    path('modificar_area/<id_area>/', modificar_area, name='modificar_area')
 ]
