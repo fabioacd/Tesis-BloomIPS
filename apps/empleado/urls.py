17edcd145f2212855f6308adf8d355bb9b72aa27
Fabio Andrés Castañeda D <castaneda.fabio@correounivalle.edu.co>
2018-09-22 13:36:25 -0500

    Se crea crud funcional de EPS

--- apps/empleado/urls.py
+++ apps/empleado/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from apps.empleado.views import agregar_area, consultar_area, modificar_area
-
+from apps.empleado.views import agregar_eps, consultar_eps, modificar_eps
 
 urlpatterns = [
     path('agregar_area/', agregar_area, name='agregar_area'),
     path('consultar_area/', consultar_area, name='consultar_area'),
-    path('modificar_area/<id_area>/', modificar_area, name='modificar_area')
+    path('modificar_area/<id_area>/', modificar_area, name='modificar_area'),
+    path('agregar_eps/', agregar_eps, name='agregar_eps'),
+    path('modificar_eps/<nit>/', modificar_eps, name='modificar_eps'),
+    path('consultar_eps/', consultar_eps, name='consultar_eps'),
 ]
