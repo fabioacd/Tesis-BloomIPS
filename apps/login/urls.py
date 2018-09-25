3eb051bc7cd3cebd947bc61beb09e3592006f010
sebascastano <castano.sebastian@correounivalle.edu.co>
2018-09-25 17:15:36 -0500

    Se arregla problema de las imagenes

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 
 from apps.login.views import login_user, bloqueo_user
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path('', login_user, name='login'),
@@ -11,6 +10,3 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
