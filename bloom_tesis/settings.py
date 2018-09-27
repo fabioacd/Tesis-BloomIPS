ce5c6ba0170fdc166e8eec9512577a87d73002bc
sebascastano <castano.sebastian@correounivalle.edu.co>
2018-09-27 17:45:36 -0500

    Se modifican archivos para restringuir vistas a usuarios por url

--- bloom_tesis/settings.py
+++ bloom_tesis/settings.py
@@ -138,3 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'media'))
+
+
+LOGIN_URL = 'login'
