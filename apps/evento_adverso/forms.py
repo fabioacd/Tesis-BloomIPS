a57f455d3361724fa9b0bbe2080a4dd16501c0c0
Fabio Andrés Castañeda D <castaneda.fabio@correounivalle.edu.co>
2018-10-15 15:17:07 -0500

    Se agrega modificar y consultar implicado a evento adverso

--- apps/evento_adverso/forms.py
+++ apps/evento_adverso/forms.py
@@ -25,8 +25,6 @@ class AgregarImplicadoEventoForm(forms.ModelForm):
     def clean(self):
         form_data = super().clean()
         edad = self.cleaned_data['edad']
-        print(edad)
-
         if( edad < 0):
             raise forms.ValidationError('La edad no puede ser negativa')
         else:
@@ -34,7 +32,34 @@ class AgregarImplicadoEventoForm(forms.ModelForm):
 
         return form_data
 
+    class Meta:
+        model = ImplicadoEvento
+        fields = ('nombre', 'id_implicado', 'edad', 'seguridad_social')
 
+
+class ModificarImplicadoEventoAdversoForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ModificarImplicadoEventoAdversoForm, self).__init__(*args, **kwargs)
+        # self.fields['imagen'].required = False
+        self.fields['nombre'].widget.attrs['class'] = "alphabetic"
+        self.fields['id_implicado'].widget.attrs['class'] = "numeric"
+        self.fields['edad'].widget.attrs['class'] = "numeric"
+        # Placeholders
+        self.fields['nombre'].widget.attrs['placeholder'] = "Ej. Luis Felipe Gómez Castrillon"
+        self.fields['id_implicado'].widget.attrs['placeholder'] = "Ej. 1144958632"
+        self.fields['seguridad_social'].widget.attrs['placeholder'] = "Ej. Coomeva EPS"
+        self.fields['edad'].widget.attrs['placeholder'] = "Ej. 28"
+
+    def clean(self):
+        form_data = super().clean()
+        edad = self.cleaned_data['edad']
+        if (edad < 0):
+            raise forms.ValidationError('La edad no puede ser negativa')
+        else:
+            pass
+
+        return form_data
 
     class Meta:
         model = ImplicadoEvento
--- apps/evento_adverso/views.py
+++ apps/evento_adverso/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib import messages
 import json
-from .forms import AgregarImplicadoEventoForm
+from .forms import AgregarImplicadoEventoForm, ModificarImplicadoEventoAdversoForm
+from .models import ImplicadoEvento
 
 def index(request):
     return render(request, 'evento_adverso/index.html')
+
+#---------------------IMPLICADO A EVENTO ADVERSO-----------------------------------------------------
 
 def agregar_implicado_evento_adverso(request):
     form_crear_implicado = AgregarImplicadoEventoForm()
@@ -39,3 +42,28 @@ def agregar_implicado_evento_adverso(request):
 
     contexto = {'form_crear_implicado': form_crear_implicado}
     return render(request, 'implicado/agregar_implicado_evento_adverso.html', contexto)
+
+def modificar_implicado_evento_adverso(request, id_implicado):
+    implicado = ImplicadoEvento.objects.get(id_implicado = id_implicado)
+
+    if request.method == 'POST':
+        form = ModificarImplicadoEventoAdversoForm(request.POST, instance = implicado)
+
+        if form.is_valid():
+            form.save()
+            form = ModificarImplicadoEventoAdversoForm(instance=implicado)
+            messages.success(request, 'Implicado a evento modificado exitosamente')
+            return redirect(consultar_implicados_evento_adverso)
+        else:
+            messages.error(request, 'No se pudo modificar el implicado')
+
+    else:
+        form = ModificarImplicadoEventoAdversoForm(instance = implicado)
+
+    contexto = {'form': form, 'implicado': implicado}
+    return render(request,'implicado/modificar_implicado_evento_adverso.html', contexto)
+
+def consultar_implicados_evento_adverso(request):
+    implicados = list(ImplicadoEvento.objects.all())
+    contexto = {'implicados': implicados}
+    return render(request, 'implicado/consultar_implicado_evento_adverso.html', contexto)
