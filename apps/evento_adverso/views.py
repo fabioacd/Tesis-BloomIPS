1bf52ebae8cead963162f9e499cbb829da65d070
Fabio Andrés Castañeda Duarte <fcastaneda@softdreams.co>
2018-11-06 14:12:51 -0500

    Se agrega evento adverso, consultar evento adverso, modificar evento adverso. Templates y treeview funcionales

--- apps/evento_adverso/views.py
+++ apps/evento_adverso/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib import messages
 import json
-from .forms import AgregarImplicadoEventoForm, ModificarImplicadoEventoAdversoForm
-from .models import ImplicadoEvento
+from .forms import AgregarImplicadoEventoForm, ModificarImplicadoEventoAdversoForm, AgregarEventoAdversoForm, ModificarEventoAdversoForm
+from .models import ImplicadoEvento, EventoAdverso
 
 def index(request):
     return render(request, 'evento_adverso/index.html')
@@ -67,3 +67,45 @@ def consultar_implicados_evento_adverso(request):
     implicados = list(ImplicadoEvento.objects.all())
     contexto = {'implicados': implicados}
     return render(request, 'implicado/consultar_implicado_evento_adverso.html', contexto)
+
+
+#---------------------EVENTO ADVERSO-----------------------------------------------------
+
+def agregar_evento_adverso(request):
+    form_evento_adverso = AgregarEventoAdversoForm()
+    if request.method == 'POST':
+        form_evento_adverso = AgregarEventoAdversoForm(request.POST)
+        if form_evento_adverso.is_valid():
+            form_evento_adverso.save()
+            messages.success(request, 'se agregó el evento adverso')
+            return redirect('consultar_evento')
+        else:
+            print(form_evento_adverso.errors)
+            messages.error(request, 'Error al registrar el evento adverso, intente nuevamente')
+
+    contexto = {'form_evento_adverso':form_evento_adverso}
+    return render(request,'evento_adverso/agregar_evento_adverso.html', contexto)
+
+def consultar_evento(request):
+    eventos = list(EventoAdverso.objects.all())
+    contexto = {'eventos': eventos}
+    return render(request, 'evento_adverso/consultar_evento_adverso.html', contexto)
+
+def modificar_evento(request, id_evento):
+    evento = EventoAdverso.objects.get(id = id_evento)
+
+    if request.method == 'POST':
+        form = ModificarEventoAdversoForm(request.POST, instance = evento)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Evento modificado exitosamente')
+            return redirect(consultar_evento)
+        else:
+            messages.error(request, 'No se pudo modificar el evento')
+
+    else:
+        form = ModificarEventoAdversoForm(instance = evento)
+
+    contexto = {'form': form, 'evento': evento}
+    return render(request,'evento_adverso/modificar_evento_adverso.html', contexto)
